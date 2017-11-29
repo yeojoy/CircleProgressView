e9ea783204fdac634b2f5b95a6508f8988e75ecb
yeojoy <yeojoy.kim@gmail.com>
2017-11-29 22:45:06 +0900

    attrs 정의 및 적용

--- app/src/main/java/me/yeojoy/circleprogressview/widget/CircleProgressView.java
+++ app/src/main/java/me/yeojoy/circleprogressview/widget/CircleProgressView.java
@@ -2,15 +2,20 @@ package me.yeojoy.circleprogressview.widget;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+
+import me.yeojoy.circleprogressview.R;
 
 /**
  * Created by yeojoy on 2017. 11. 28..
@@ -19,13 +24,23 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 public class CircleProgressView extends View {
     private static final String TAG = CircleProgressView.class.getSimpleName();
 
+    private static final float DEFAULT_SHADOW_SIZE = 10f;
+    private static final int DEFAULT_STYLE = 0; /* Default style >> STROKE */
+    private static final long DEFAULT_ANIMTION_DURATION = 1000L; /* Default style >> STROKE */
+
     private float mLineWidth, mBgLineWidth;
 
     private int mPercent;
     private int mCircleColor, mBackgroundCircleColor;
     private Paint mPaint, mBackgroundPaint;
 
-    private RectF mRectF, mLineRectF;
+    private RectF mRectF;
+
+    private boolean mHasShadow;
+    private int mStyle;
+
+    private long mAnimationDuration;
+    private boolean mHasAnimation;
 
     public CircleProgressView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -35,31 +50,53 @@ public class CircleProgressView extends View {
 
     private void initView(AttributeSet attributeSet) {
         if (attributeSet == null) {
-
+            return;
         }
 
-        mPercent = 0;
+        Context context = getContext();
+        Resources resources = context.getResources();
+        TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.CircleProgressView);
 
-        mLineWidth = getContext().getResources().getDisplayMetrics().density * 3;
-        mBgLineWidth = getContext().getResources().getDisplayMetrics().density * 5;
-        mCircleColor = Color.RED;
-        mBackgroundCircleColor = Color.BLACK;
+        mHasShadow = typedArray.getBoolean(R.styleable.CircleProgressView_has_shadow, false);
+        mLineWidth = typedArray.getDimensionPixelSize(R.styleable.CircleProgressView_line_width,
+                resources.getDimensionPixelSize(R.dimen.default_line_width));
+        mBgLineWidth = typedArray.getDimensionPixelSize(R.styleable.CircleProgressView_background_line_width,
+                resources.getDimensionPixelSize(R.dimen.default_background_line_width));
+
+        mCircleColor = typedArray.getColor(R.styleable.CircleProgressView_line_color,
+                ContextCompat.getColor(context, R.color.default_line_color));
+        mBackgroundCircleColor = typedArray.getColor(R.styleable.CircleProgressView_background_line_color,
+                ContextCompat.getColor(context, R.color.default_background_line_color));
+
+        mStyle = typedArray.getInt(R.styleable.CircleProgressView_style, DEFAULT_STYLE);
+
+        mHasAnimation = typedArray.getBoolean(R.styleable.CircleProgressView_has_animation, true);
+        mAnimationDuration = typedArray.getInt(R.styleable.CircleProgressView_animation_duration, (int) DEFAULT_ANIMTION_DURATION);
+
+        // Background line width가 크다면 line width로 변경
+        if (mLineWidth > mBgLineWidth) {
+            mLineWidth = mBgLineWidth;
+        }
 
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setColor(mCircleColor);
-        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStyle(mStyle == DEFAULT_STYLE ? Paint.Style.STROKE : Paint.Style.FILL);
         mPaint.setStrokeWidth(mLineWidth);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        // 선 끝을 둥글게 만듬
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
 
         mBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mBackgroundPaint.setColor(mBackgroundCircleColor);
-        mBackgroundPaint.setStyle(Paint.Style.STROKE);
+        mBackgroundPaint.setStyle(mStyle == DEFAULT_STYLE ? Paint.Style.STROKE : Paint.Style.FILL);
         mBackgroundPaint.setStrokeWidth(mBgLineWidth);
-        mBackgroundPaint.setShadowLayer(3f, 10f, 10f, Color.parseColor("#80330000"));
+        mBackgroundPaint.setShadowLayer(3f, DEFAULT_SHADOW_SIZE, DEFAULT_SHADOW_SIZE,
+                Color.parseColor("#80330000"));
 
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        mPercent = 40;
-//        animateCircleProgress(90);
+        mPercent = typedArray.getInt(R.styleable.CircleProgressView_value, 0);
+
+        // Shadow가 있을 때 추가 함.
+        setLayerType(mHasShadow ? View.LAYER_TYPE_SOFTWARE : View.LAYER_TYPE_NONE, null);
+        animateCircleProgress(mPercent);
     }
 
     @Override
@@ -77,8 +114,8 @@ public class CircleProgressView extends View {
         }
 
 //        canvas.drawCircle(cX, cY, cX, mBackgroundPaint);
-        canvas.drawArc(mRectF, 270, 360, false, mBackgroundPaint);
-        canvas.drawArc(mRectF, 270, sweepDegree, false, mPaint);
+        canvas.drawArc(mRectF, 270, 360, mStyle != DEFAULT_STYLE, mBackgroundPaint);
+        canvas.drawArc(mRectF, 270, sweepDegree, mStyle != DEFAULT_STYLE, mPaint);
 
     }
 
@@ -102,24 +139,15 @@ public class CircleProgressView extends View {
             mRectF = new RectF();
         }
 
-        if (mLineRectF == null) {
-            mLineRectF = new RectF();
-        }
+        mRectF.left = 0 + (mBgLineWidth / 2) + (mHasShadow ? DEFAULT_SHADOW_SIZE : 0);
 
-        mRectF.left = 0 + (mBgLineWidth / 2) + 10;
-//        mLineRectF.left = mRectF.left + ((mBgLineWidth - mLineWidth) / 2);
+        mRectF.top = 0 + (mBgLineWidth / 2) + (mHasShadow ? DEFAULT_SHADOW_SIZE : 0);
 
-        mRectF.top = 0 + (mBgLineWidth / 2) + 10;
-//        mLineRectF.top = mRectF.top + ((mBgLineWidth - mLineWidth) / 2);
+        mRectF.right = right - left - mRectF.left - (mBgLineWidth / 2) - (mHasShadow ? DEFAULT_SHADOW_SIZE : 0);
 
-        mRectF.right = right - left - mRectF.left - (mBgLineWidth / 2) - 10;
-//        mLineRectF.right = mRectF.right - ((mBgLineWidth - mLineWidth) / 2);
-
-        mRectF.bottom = bottom - top - (mBgLineWidth / 2) - 10;
-//        mLineRectF.bottom = mRectF.bottom - ((mBgLineWidth - mLineWidth) / 2);
+        mRectF.bottom = bottom - top - (mBgLineWidth / 2) - (mHasShadow ? DEFAULT_SHADOW_SIZE : 0);
 
         Log.d(TAG, "onLayout() > mRectF >>> " + mRectF.toString());
-//        Log.d(TAG, "onLayout() > mLineRectF >>> " + mLineRectF.toString());
     }
 
     public void animateCircleProgress(final int percent) {
@@ -128,25 +156,29 @@ public class CircleProgressView extends View {
         mPercent = 0;
         invalidate();
 
-        final long time = (long) (800L * percent * 0.01);
-        Log.d(TAG, "Animation Time : " + time);
-
         postDelayed(new Runnable() {
             @Override
             public void run() {
                 Log.i(TAG, "run()");
-                ValueAnimator valueAnimator = ValueAnimator.ofInt(0, percent);
-                valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-                valueAnimator.setDuration(time);
-                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator animation) {
-                        int currentValue = ((int) animation.getAnimatedValue());
-                        mPercent = currentValue;
-                        invalidate();
-                    }
-                });
-                valueAnimator.start();
+                if (mHasAnimation) {
+                    final long time = (long) (mAnimationDuration * percent * 0.01);
+                    Log.d(TAG, "Animation Time : " + time);
+                    ValueAnimator valueAnimator = ValueAnimator.ofInt(0, percent);
+                    valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+                    valueAnimator.setDuration(time);
+                    valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            int currentValue = ((int) animation.getAnimatedValue());
+                            mPercent = currentValue;
+                            invalidate();
+                        }
+                    });
+                    valueAnimator.start();
+                } else {
+                    mPercent = percent;
+                    invalidate();
+                }
             }
         }, 300L);
     }
